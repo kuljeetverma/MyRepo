d764dccf7cae9be10baaed20f265985f29943b26
kuljeetverma <kuljeet.verma@nagarro.com>
2018-11-02 12:41:37 +0530

    logger added

--- hello.py
+++ hello.py
@@ -1,2 +1,23 @@
-def say_hello():
-	print "Hello, Kuljeet Verma"
+import logging
+import os
+ 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+
+logger_handler = logging.FileHandler('python_logging.log')
+logger_handler.setLevel(logging.WARNING)
+
+logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+
+logger_handler.setFormatter(logger_formatter)
+
+logger.addHandler(logger_handler)
+
+
+def say_hello(name):
+	logger.warning("function called")
+	logger.warning("Greetings from {}".format(name))
+
+
+if __name__ == '__main__':
+	say_hello("Kuljeet")
